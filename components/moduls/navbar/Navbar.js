480e0b805fd2dd37478973ce1e5d28410c47482d
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-05 13:19:50 +0100

    develop navbar

--- components/moduls/navbar/Navbar.js
+++ components/moduls/navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <nav
         className={`${styles.navbar} ${styles.navbar_expand_lg} bg-none navbar-dark py-3`}
       >
-        <a href="index.html" class={`${styles.navbar_brand} px-lg-4 m-0`}>
+        <a href="/" class={`${styles.navbar_brand} px-lg-4 m-0`}>
           <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
         </a>
         <button
@@ -24,18 +24,18 @@ function Navbar() {
         >
           <div className={`${styles.navbar_nav} ml-auto p-4`}>
             <a
-              href="index.html"
+              href="/"
               className={`${styles.nav_link} ${styles.active_nav_link}`}
             >
               Home
             </a>
-            <a href="about.html" className={`${styles.nav_link}`}>
+            <a href="/about" className={`${styles.nav_link}`}>
               About
             </a>
-            <a href="service.html" className={`${styles.nav_link}`}>
+            <a href="/service" className={`${styles.nav_link}`}>
               Service
             </a>
-            <a href="menu.html" className={`${styles.nav_link}`}>
+            <a href="/menu" className={`${styles.nav_link}`}>
               Menu
             </a>
             <div class={`${styles.dropdown}`}>
@@ -47,15 +47,15 @@ function Navbar() {
                 Pages
               </a>
               <div className={`${styles.dropdown_menu} ${styles.text_capitalize}`}>
-                <a href="reservation.html" className={`${styles.dropdown_item}`}>
+                <a href="/reservation" className={`${styles.dropdown_item}`}>
                   Reservation
                 </a>
-                <a href="testimonial.html" className={`${styles.dropdown_item}`}>
+                <a href="/testimonial" className={`${styles.dropdown_item}`}>
                   Testimonial
                 </a>
               </div>
             </div>
-            <a href="contact.html" className={`${styles.nav_link}`}>
+            <a href="/contact" className={`${styles.nav_link}`}>
               Contact
             </a>
           </div>
