df754b373b8922240da4858f789693123b7872ea
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-05 11:45:29 +0100

    develop navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,11 @@
+import Navbar from "@/Components/moduls/navbar/Navbar";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css"
 export default function App({ Component, pageProps }) {
   return (
     <>
-    
-    <div className="container-fluid p-0 nav-bar">
-        <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3">
-            <a href="index.html" className="navbar-brand px-lg-4 m-0">
-                <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
-            </a>
-            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                <div className="navbar-nav ml-auto p-4">
-                    <a href="/" className="nav-item nav-link active">Home</a>
-                    <a href="/about" className="nav-item nav-link">About</a>
-                    <a href="service" className="nav-item nav-link">Service</a>
-                    <a href="menu" className="nav-item nav-link">Menu</a>
-                   
-                    
-                     
-                            <a href="/reservation" className="nav-item nav-link">Reservation</a>
-                            <a href="/testimonial" className="nav-item nav-link">Testimonial</a>
-                    
-                   {/*  <div className="nav-item dropdown">
-                        <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                        <div className="dropdown-menu text-capitalize">
-                            <a href="/reservation" className="dropdown-item">Reservation</a>
-                            <a href="/testimoniall" className="dropdown-item">Testimonial</a>
-                        </div>
-                    </div> */}
-                    <a href="contact" className="nav-item nav-link">Contact</a>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <Navbar/>
+   
   
     <div className="container-fluid p-0 mb-5">
         <div id="blog-carousel" className="carousel slide overlay-bottom" data-ride="carousel">
