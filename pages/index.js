7f64324a2def20eb1117b0635721585bf0c1a90f
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 10:48:11 +0100

    componnent Testmonial in index-page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import About from "@/Components/templates/index/About";
 import Menu from "@/Components/templates/index/Menu";
 import Offer from "@/Components/templates/index/Offer";
+import Reservation from "@/Components/templates/index/Reservation";
 import Service from "@/Components/templates/index/Service";
 import Slider from "@/Components/templates/index/Slider";
+import Testimonial from "@/Components/templates/index/Testimonial";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
  <Service/>
  <Offer/>
  <Menu/>
+ <Reservation/>
+ <Testimonial/>
     </>
 
   )
