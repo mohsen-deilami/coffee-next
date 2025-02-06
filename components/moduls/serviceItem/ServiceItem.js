07c08f209d25071b4987ba950a95e49a655fef77
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 16:03:02 +0100

    upgrade service componnent

--- components/moduls/serviceItem/ServiceItem.js
+++ components/moduls/serviceItem/ServiceItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function ServiceItem() {
+export default function ServiceItem({title , desc , img , icon}) {
   return (
-    <div>
-      ServiceItem
+    <div className="col-lg-6 mb-5">
+    <div className="row align-items-center">
+        <div className="col-sm-5">
+            <img className="img-fluid mb-3 mb-sm-0" src={img} alt=""/>
+        </div>
+        <div className="col-sm-7">
+            <h4><i className="fa fa-truck service-icon"></i>  {title}</h4>
+            <p className="m-0">{desc}</p>
+        </div>
     </div>
+</div>
   )
 }
