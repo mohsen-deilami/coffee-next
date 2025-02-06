40f35fab95b78ca82c4225f1706649c4bbb602d1
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 11:56:41 +0100

    develop menu page

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,9 +1,20 @@
+import Menu from '@/Components/templates/index/Menu'
 import React from 'react'
 
 export default function index() {
   return (
     <div>
-      menu page
+    <div className="container-fluid page-header mb-5 position-relative overlay-bottom">
+        <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{minHeight: "400px"}}>
+            <h1 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Menu</h1>
+            <div className="d-inline-flex mb-lg-5">
+                <p className="m-0 text-white"><a className="text-white" href="/">Home</a></p>
+                <p className="m-0 text-white px-2">/</p>
+                <p className="m-0 text-white">Menu</p>
+            </div>
+        </div>
+    </div>
+    <Menu/>
     </div>
   )
 }
