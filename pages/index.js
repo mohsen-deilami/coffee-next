b0bf71d7d176f6a976e3de0c25c467cc6b83087d
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 10:28:54 +0100

    componnent Offer in index-page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import About from "@/Components/templates/index/About";
+import Offer from "@/Components/templates/index/Offer";
 import Service from "@/Components/templates/index/Service";
 import Slider from "@/Components/templates/index/Slider";
 
@@ -8,6 +9,7 @@ export default function Home() {
  <Slider/>
  <About/>
  <Service/>
+ <Offer/>
     </>
 
   )
