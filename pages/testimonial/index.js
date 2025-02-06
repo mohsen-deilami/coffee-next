e8c20dd26790799df3c8bab292d52bef105b7caa
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 12:02:55 +0100

    develop testmoniatial page

--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,7 +1,20 @@
+import Testimonial from '@/Components/templates/index/Testimonial'
 import React from 'react'
 
 export default function index() {
   return (
- <p>test page</p>
+<div>
+<div class="container-fluid page-header mb-5 position-relative overlay-bottom">
+        <div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{minHeight: "400px"}}>
+            <h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Testimonial</h1>
+            <div class="d-inline-flex mb-lg-5">
+                <p class="m-0 text-white"><a class="text-white" href="/">Home</a></p>
+                <p class="m-0 text-white px-2">/</p>
+                <p class="m-0 text-white">Testimonial</p>
+            </div>
+        </div>
+    </div>
+    <Testimonial/>
+</div>
   )
 }
