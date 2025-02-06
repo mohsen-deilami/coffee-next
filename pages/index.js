a378b20c0bb99baaf6fbd6829b2171af2a265a36
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-06 10:33:35 +0100

    componnent Menu in index-page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import About from "@/Components/templates/index/About";
+import Menu from "@/Components/templates/index/Menu";
 import Offer from "@/Components/templates/index/Offer";
 import Service from "@/Components/templates/index/Service";
 import Slider from "@/Components/templates/index/Slider";
@@ -10,6 +11,7 @@ export default function Home() {
  <About/>
  <Service/>
  <Offer/>
+ <Menu/>
     </>
 
   )
