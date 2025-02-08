fc840625e33d6f41b76318d822f26c6b4920c0af
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-08 09:51:14 +0100

    edit header-page

--- components/moduls/pageHeader/PageHeader.js
+++ components/moduls/pageHeader/PageHeader.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function PageHeader() {
+export default function PageHeader({pageName}) {
   return (
-    <div>
-      PageHeader
+    <div className="container-fluid page-header mb-5 position-relative overlay-bottom">
+        <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{minHeight: "400px"}}>
+            <h1 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase"> {pageName}</h1>
+            <div className="d-inline-flex mb-lg-5">
+                <p className="m-0 text-white"><a className="text-white" href="/">Home</a></p>
+                <p className="m-0 text-white px-2">/</p>
+                <p className="m-0 text-white"> {pageName}</p>
+            </div>
+        </div>
     </div>
   )
 }
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,19 +1,11 @@
+import PageHeader from '@/Components/moduls/pageHeader/PageHeader'
 import Menu from '@/Components/templates/index/Menu'
 import React from 'react'
 
 export default function index() {
   return (
     <div>
-    <div className="container-fluid page-header mb-5 position-relative overlay-bottom">
-        <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{minHeight: "400px"}}>
-            <h1 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Menu</h1>
-            <div className="d-inline-flex mb-lg-5">
-                <p className="m-0 text-white"><a className="text-white" href="/">Home</a></p>
-                <p className="m-0 text-white px-2">/</p>
-                <p className="m-0 text-white">Menu</p>
-            </div>
-        </div>
-    </div>
+  <PageHeader pageName={'Menu'}/>
     <Menu/>
     </div>
   )
