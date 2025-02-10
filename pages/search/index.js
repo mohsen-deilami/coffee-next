0214e6cdc1fbe6e5fb2f745a4040d5ec12f153d7
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-10 21:44:46 +0100

    create search page

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageHeader from '@/Components/moduls/pageHeader/PageHeader'
+import Result from '@/Components/templates/search/Result'
 
 export default function index() {
   return (
     <div>
        <PageHeader pageName={"Search"} />
+       <Result/>
     </div>
   )
 }
