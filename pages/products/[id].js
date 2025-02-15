cffc5ed1c0f912ac00edf0dce801181e115d16cc
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-15 21:35:03 +0100

    develop comments in detail page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -3,7 +3,8 @@ import ProductDetail from "@/Components/templates/product/ProductDetail";
 import Comments from "@/Components/templates/testimonial/Comments";
 import React from "react";
 
-export default function detail({ menuData }) {
+export default function detail({ menuData , commentsData }) {
+  console.log(commentsData);
   return (
     <div>
       <PageHeader pageName={"Detail"} />
@@ -12,7 +13,7 @@ export default function detail({ menuData }) {
     </div>
   );
 }
-export async function getStaticPaths({ context }) {
+export async function getStaticPaths() {
   const menuResponse = await fetch("http://localhost:4000/menu");
   const menuData = await menuResponse.json();
   const paths = menuData.map((item) => ({ params: { id: String(item.id) } }));
@@ -26,10 +27,13 @@ export async function getStaticProps(context) {
   const { params } = context;
   const menuResponse = await fetch(`http://localhost:4000/menu/${params.id}`);
   const menuData = await menuResponse.json();
-  console.log(menuData);
+  const commentsResponse = await fetch(`http://localhost:4000/comments`);
+  const commentsData = await commentsResponse.json();
+console.log(commentsData)
   return {
     props: {
       menuData,
+      commentsData
     },
   };
 }
