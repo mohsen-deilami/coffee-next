fb35d8d329c878e343b699821e0dad9d0d0fe6f1
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-02-15 23:20:02 +0100

    develop comments in detail page

--- components/templates/testimonial/Comments.js
+++ components/templates/testimonial/Comments.js
@@ -1,15 +1,69 @@
+import styles from "@/styles/Product.module.css";
 
-import React from "react";
-import Testimonial from "../index/Testimonial";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function Comments({comments}) {
-  console.log(comments);
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+const Comments = ({ comments }) => {
   return (
-    <>
-     <h1>comments</h1> 
-     
-    </>
+    <div className="container-fluid pt-5">
+      <div className="container">
+        <div className="section-title">
+          <h4
+            className="text-primary text-uppercase"
+            style={{ letterSpacing: "5px" }}
+          >
+            TESTIMONIAL
+          </h4>
+          <h1 className="display-4">Product Comments</h1>
+        </div>
+        <div className="row">
+          <Swiper
+            loop={true}
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              1024: {
+                slidesPerView: 2,
+                spaceBetween: 100,
+              },
+            }}
+            modules={[Pagination]}
+            className={styles.swiper}
+          >
+            {comments.slice(0, 6).map((comment) => (
+              <SwiperSlide className={styles.swiper_slide} key={comment.id}>
+                <div className="testimonial-item">
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      className="img-fluid"
+                      width={100}
+                      src={comment.profile}
+                      alt=""
+                    />
+                    <div className="ml-3">
+                      <h4>{comment.username}</h4>
+                      <p className="text-left mb-0">User</p>
+                    </div>
+                  </div>
+                  <p className="m-0 mb-4">{comment.body}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-
+export default Comments;
